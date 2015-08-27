45b23a7b76d47763d4e47d9da7a918d1c28117e6
Melanie Murray <murray.melanief@gmail.com>
2015-08-27 10:33:15 -0500

    'HopeSpringsEternal'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HopeSpringsEternal::Application.config.secret_token = '6ad8b1ad88b3832e4326e70fcd04412b3ebfe5531fb39a274dfcbfdbff1fd6aaf16aab254ecb8ede49320f49d493b96dbc60b831f2a084b205ee428a9471d44c'
